71c99ac97b064e362224c725c3e83bd98684f276
dexzhu1987 <dexzhu1987@hotmail.com>
2018-08-21 14:38:00 -0700

    firebase user auth added

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,40 +1,53 @@
 // import firebase from 'react-native-firebase'
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   TextInput,
   Button,
   KeyboardAvoidingView,
   Text,
-  TouchableOpacity,
-} from 'react-native';
-import { Constants } from 'expo';
-
-const ERRORMILSECONDS = 3000;
+  TouchableOpacity
+} from "react-native";
+import { Constants } from "expo";
+import firebase from "firebase";
+const ERRORMILSECONDS = 4000;
 
 export default class LoginScreen extends React.Component {
   state = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     isFormValid: false,
     error: false,
-    errorMessage: '',
+    errorMessage: ""
   };
+
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyBbZa3eNn-RvkCrppkVL4fmwjHwEID6dQg",
+      authDomain: "reactnativefinalsigninproject.firebaseapp.com",
+      databaseURL: "https://reactnativefinalsigninproject.firebaseio.com",
+      projectId: "reactnativefinalsigninproject",
+      storageBucket: "reactnativefinalsigninproject.appspot.com",
+      messagingSenderId: "609800797128"
+    };
+    firebase.initializeApp(config);
+  }
 
   handleLogin = () => {
     const { email, password } = {
       email: this.state.email,
-      password: this.state.password,
+      password: this.state.password
     };
-    // firebase
-    //   .auth()
-    //   .signInWithEmailAndPassword(email, password)
-    //   .then(() => this.props.navigation.navigate('Setting'))
-    //   .catch(error => this.setState({ errorMessage: error.message, error: true }))
-    //   setTimeout(() => {
-    //     this.setState({ error: false });
-    //   }, ERRORMILSECONDS);
-    this.props.navigation.navigate('Setting');
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => this.props.navigation.navigate("Setting"))
+      .catch(error =>
+        this.setState({ errorMessage: error.message, error: true })
+      );
+    setTimeout(() => {
+      this.setState({ error: false });
+    }, ERRORMILSECONDS);
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -82,17 +95,25 @@ export default class LoginScreen extends React.Component {
         {this.state.error && (
           <Text
             style={{
-              textAlignVertical: 'center',
-              textAlign: 'center',
-              color: '#800',
-            }}>
+              textAlignVertical: "center",
+              textAlign: "center",
+              color: "#800"
+            }}
+          >
             {this.state.errorMessage}
           </Text>
         )}
         <TouchableOpacity
           disabled={!this.state.isFormValid}
-          onPress={this.handleLogin}>
-          <Text style={this.state.isFormValid? styles.button :styles.buttonInValid}>Login</Text>
+          onPress={this.handleLogin}
+        >
+          <Text
+            style={
+              this.state.isFormValid ? styles.button : styles.buttonInValid
+            }
+          >
+            Login
+          </Text>
         </TouchableOpacity>
       </KeyboardAvoidingView>
     );
@@ -102,43 +123,43 @@ export default class LoginScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: Constants.statusBarHeight,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   input: {
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     minWidth: 100,
     marginTop: 20,
     marginHorizontal: 20,
     paddingHorizontal: 10,
     paddingVertical: 5,
-    borderRadius: 3,
+    borderRadius: 3
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e"
   },
   button: {
     margin: 24,
     padding: 10,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-    backgroundColor: '#88ba1b'
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e",
+    backgroundColor: "#88ba1b"
   },
   buttonInValid: {
     margin: 24,
     padding: 10,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-    backgroundColor: '#8f997b'
-  },
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e",
+    backgroundColor: "#8f997b"
+  }
 });
